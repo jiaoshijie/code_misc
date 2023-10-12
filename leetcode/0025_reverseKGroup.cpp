74b390d5e87701c1de826a689c96d22f1c467060
jiaoshijie <jiao1751959040@gmail.com>
2023-10-12 16:03:59 +0800

    sync

--- leetcode/0025_reverseKGroup.cpp
+++ leetcode/0025_reverseKGroup.cpp
@@ -11,51 +11,95 @@ struct ListNode {
 
 // TODO: 92.57% "代码写的有点乱有时间整理整理" write a article
 
+// class Solution {
+// public:
+//   ListNode* reverseKGroup(ListNode* head, int k) {
+//     if (k == 1)
+//       return head;
+//     ListNode* p = head, *q = head, *lend, *nbegin = head, *temp = nullptr;
+//     for (int i = 1; i < k; i++) {
+//       if (q == nullptr || q->next == nullptr) return head;
+//       q = q->next;
+//     }
+//     nbegin = q->next;
+//     temp = reverseList(p, q);
+//     ListNode *res = temp;
+//     lend = p;
+//     p = nbegin;
+//     q = nbegin;
+
+//     while (p && q) {
+//       for (int i = 1; i < k; i++) {
+//         if (q == nullptr || q->next == nullptr) {
+//           lend->next = nbegin;
+//           return res;
+//         }
+//         q = q->next;
+//       }
+//       nbegin = q->next;
+//       temp = reverseList(p, q);
+//       lend->next = temp;
+//       lend = p;
+//       p = nbegin;
+//       q = nbegin;
+//     }
+//     return res;
+//   }
+
+//   ListNode *reverseList(ListNode *begin, ListNode *end) {
+//     ListNode *head = begin, *cur = begin->next, *temp = cur->next;
+//     while (head != end) {
+//       temp = cur->next;
+//       cur->next = head;
+//       head = cur;
+//       cur = temp;
+//     }
+//     begin->next = nullptr;
+//     return end;
+//   }
+
+//   void PrintL(ListNode *head) {
+//     ListNode *p = head;
+//     while (p != nullptr) {
+//       std::cout << p->val << ' ';
+//       p = p->next;
+//     }
+//     std::cout << std::endl;
+//   }
+// };
+
+// New version
 class Solution {
 public:
   ListNode* reverseKGroup(ListNode* head, int k) {
-    if (k == 1)
-      return head;
-    ListNode* p = head, *q = head, *lend, *nbegin = head, *temp = nullptr;
-    for (int i = 1; i < k; i++) {
-      if (q == nullptr || q->next == nullptr) return head;
-      q = q->next;
-    }
-    nbegin = q->next;
-    temp = reverseList(p, q);
-    ListNode *res = temp;
-    lend = p;
-    p = nbegin;
-    q = nbegin;
+    if (k == 1 || !head) return head;
+    ListNode *p = head, *q = head, *ret = head, *prev_end = nullptr;
 
-    while (p && q) {
-      for (int i = 1; i < k; i++) {
-        if (q == nullptr || q->next == nullptr) {
-          lend->next = nbegin;
-          return res;
-        }
+    for(;;) {
+      int tk = 1;
+      while (q && tk < k) {
         q = q->next;
+        tk++;
       }
-      nbegin = q->next;
-      temp = reverseList(p, q);
-      lend->next = temp;
-      lend = p;
-      p = nbegin;
-      q = nbegin;
-    }
-    return res;
-  }
+      if (!(tk >= k) || !q) break;
+      if (ret == head) ret = q;
+      if (prev_end) prev_end->next = q;
 
-  ListNode *reverseList(ListNode *begin, ListNode *end) {
-    ListNode *head = begin, *cur = begin->next, *temp = cur->next;
-    while (head != end) {
-      temp = cur->next;
-      cur->next = head;
-      head = cur;
-      cur = temp;
+      // reverse
+      ListNode *temp = q->next, *l = p, *r = p->next;
+      while (r != temp) {
+        ListNode *t = r->next;
+        r->next = l;
+        l = r;
+        r = t;
+      }
+      p->next = temp;
+      prev_end = p;
+      p = temp;
+      q = temp;
     }
-    begin->next = nullptr;
-    return end;
+
+    return ret;
   }
 
   void PrintL(ListNode *head) {
@@ -70,19 +114,8 @@ public:
 
 int main() {
   Solution s;
-  // ListNode *head = new ListNode(
-  //     1,
-  //     new ListNode(
-  //         2, new ListNode(
-  //                3, new ListNode(
-  //                       4, new ListNode(
-  //                              5, new ListNode(
-  //                                     6, new ListNode(
-  //                                            7, new ListNode(
-  //                                                   8, new ListNode(9)))))))));
-  ListNode *head = new ListNode(1);
-  ListNode* res = s.reverseKGroup(head, 1);
-  // ListNode *res = s.reverseList(head, head->next->next->next);
+  ListNode *head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, nullptr)))));
+  ListNode* res = s.reverseKGroup(head, 3);
   s.PrintL(res);
   return 0;
 }
--- leetcode/0718_findLength.cpp
+++ leetcode/0718_findLength.cpp
@@ -5,11 +5,36 @@ using std::vector;
 class Solution {
 public:
   int findLength(vector<int>& nums1, vector<int>& nums2) {
-    // TODO
+    vector<vector<int>> dp(nums1.size(), vector<int>(nums2.size(), 0));
+    int ret = 0;
+    for (size_t i = 0; i < nums1.size(); ++i) {
+      if (nums1[i] == nums2[0]) {
+        dp[i][0] = 1;
+        ret = 1;
+      }
+    }
+    for (size_t i = 0; i < nums2.size(); ++i) {
+      if (nums2[i] == nums1[0]) {
+        dp[0][i] = 1;
+        ret = 1;
+      }
+    }
+
+    for (size_t i = 1; i < nums1.size(); ++i) {
+      for (size_t j = 1; j < nums2.size(); ++j) {
+        if (nums1[i] == nums2[j]) {
+          dp[i][j] = 1 + (nums1[i - 1] == nums2[j - 1] ? dp[i - 1][j - 1] : 0);
+          ret = std::max(ret, dp[i][j]);
+        }
+      }
+    }
+    return ret;
   }
 };
 
 int main() {
   Solution s;
+  vector<int> v1{1, 1, 1, 1, 1}, v2{1, 1, 1, 1, 1};
+  std::cout << s.findLength(v1, v2) << std::endl;
   return 0;
 }
