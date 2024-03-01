21d69d9b605bbdd1a84a72f0b74447b9ec3e93c8
jiaoshijie <jiao1751959040@gmail.com>
2024-03-01 16:14:31 +0800

    update 0023_mergeKLists.cpp

--- leetcode/0023_mergeKLists.cpp
+++ leetcode/0023_mergeKLists.cpp
@@ -9,11 +9,69 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+// NOTE(make it to be linux kernal style): Using merge sort idea.
 class Solution {
 public:
-  /*
-   * 超时
-   * **/
+  ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode* head;
+    ListNode** indirect = &head;
+    while (list1 && list2) {
+      if (list1->val < list2->val) {
+        *indirect = list1;
+        list1 = list1->next;
+      } else {
+        *indirect = list2;
+        list2 = list2->next;
+      }
+      indirect = &(*indirect)->next;
+    }
+    *indirect = (ListNode*)((long long)list1 | (long long)list2);
+
+    return head;
+  }
+
+  // TODO: Why recursive version much faster then iteration version?
+  ListNode* mergeKLists(std::vector<ListNode*>& lists) {
+    if (lists.size() == 0) return nullptr;
+    return merge(lists, 0, lists.size());
+  }
+
+  ListNode* merge(std::vector<ListNode*>& lists, int begin, int end) {
+    int len = end - begin;
+    if (len < 0) return nullptr;
+    if (len == 1) return lists[begin];
+    int mid = begin + end >> 1;
+    ListNode *left = merge(lists, begin, mid);
+    ListNode *right = merge(lists, mid, end);
+    return mergeTwoLists(left, right);
+  }
+
+  // ListNode* mergeKLists(std::vector<ListNode*>& lists) {
+  //   if (lists.size() == 0) return nullptr;
+
+  //   for (int interval = 1; interval < lists.size(); interval *= 2) {
+  //     for (int i = 0; i + interval < lists.size(); i += interval * 2) {
+  //       lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+  //     }
+  //   }
+  //   return lists[0];
+  // }
+
+  // 打印ListNode
+  void PrintL(ListNode *head) {
+    ListNode *p = head;
+    while (p != nullptr) {
+      std::cout << p->val << ' ';
+      p = p->next;
+    }
+    std::cout << std::endl;
+  }
+};
+
+/*
+class Solution {
+public:
+  // 超时
   ListNode *mergeTwoLists(ListNode* l1, ListNode* l2) {
     ListNode res, *temp = &res, *p = l1, *q = l2;
     while (p && q) {
@@ -73,9 +131,6 @@ public:
     return res.next;
   }
 
-  // TODO: 考虑一些 "哈夫曼树" 是否可行
-  // ...
-
   // 打印ListNode
   void PrintL(ListNode *head) {
     ListNode *p = head;
@@ -86,6 +141,7 @@ public:
     std::cout << std::endl;
   }
 };
+*/
 
 int main() {
   ListNode *l1 = new ListNode(1, new ListNode(4, new ListNode(5)));
@@ -93,7 +149,7 @@ int main() {
   ListNode *l3 = new ListNode(2, new ListNode(6));
   std::vector<ListNode*> lists({l1, l2, l3});
   Solution s;
-  ListNode *res = s.mergeKLists1(lists);
+  ListNode *res = s.mergeKLists(lists);
   s.PrintL(res);
   return 0;
 }
