c579ec86862288cb9beb2cd317bfed15791567b1
jiaoshijie <jiao1751959040@gmail.com>
2024-04-19 23:22:03 +0800

    fix div16.c error

--- c-like/div16.c
+++ c-like/div16.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
 #include <assert.h>
+#include <stdint.h>
 
 int div16(int x) {
-    return (x + (x >> 31 & 0x1)) >> 4;
+    return (x >> 4) + (x >> 31 & !!(x & 15));
 }
 
 int main() {
@@ -10,5 +10,13 @@ int main() {
     assert(div16(33) == 33/16);
     assert(div16(-17) == -17/16);
     assert(div16(-33) == -33/16);
+    assert(div16(-34) == -34/16);
+    assert(div16(-32) == -32/16);
+    for (int i = 1; i < INT32_MAX; i++) {
+        assert(div16(i) == i / 16);
+    }
+    for (int i = -1; i > INT32_MIN; i--) {
+        assert(div16(i) == i / 16);
+    }
     return 0;
 }
