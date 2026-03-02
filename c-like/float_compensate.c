e2215ba772ad8c419798c676b93a164d714d71fe
jiaoshijie <jiao1751959040@gmail.com>
2026-03-02 21:49:52 +0800

    understand float_compensate.c

--- c-like/float_compensate.c
+++ c-like/float_compensate.c
@@ -1,30 +1,54 @@
 #include <stdio.h>
 
-// TODO(jiaoshijie): Think about why
+// f: 01001100001111101100000100000010
+// i:         10111110110000010000001000
+//            ^                       ^
+//            |                       |
 
-void sum1() {
+#define VAL1 10000
+#define VAL2 100000L
+#define VAL3 10001
+
+void sum1(int val) {
     float sum = 0.0f, corr = 0.0f;
-    for (int i = 1; i <= 10000; i++) {
+    for (int i = 1; i <= val; i++) {
         float y = i - corr;
-        float t = sum + y;
-        corr = (t - sum) - y;
+        float t = sum + y;  // may carry or drop
+        corr = t - sum - y; // get value that be rounded
         sum = t;
     }
 
     printf("Sum1 is %f\n", sum);
 }
 
-void sum2() {
+void sum2(int val) {
     float sum = 0.0f;
-    for (int i = 1; i <= 10000; i++) {
+    for (int i = 1; i <= val; i++) {
         sum += i;
     }
 
     printf("Sum2 is %f\n", sum);
 }
 
+void sum3(float val) {
+    printf("Sum3 is %f\n", (1.f + val) / 2.f * val);
+}
+
 int main() {
-    sum1();
-    sum2();
+    sum1(VAL1);  // NOTE: works
+    sum2(VAL1);
+    sum3(VAL1);
+    printf("%d\n", (1 + VAL1) * (VAL1 / 2));
+    puts("------------------------------------------------------------");
+    sum1(VAL2); // NOTE: does not work
+    sum2(VAL2);
+    sum3(VAL2);
+    printf("%ld\n", (1 + VAL2) * (VAL2 / 2));
+    puts("------------------------------------------------------------");
+    sum1(VAL3);
+    sum2(VAL3);
+    sum3(VAL3);
+    // i: 10111110110010101100011001
+    printf("%d\n", (1 + VAL3) / 2 * VAL3);
     return 0;
 }
